374def34bfe86be54a87df4e260b8e7d899d8f58
Hyemin Lee <cotloz88@gmail.com>
2023-07-14 06:32:15 +0000

    enable server

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// // express 서버 포트 지정
+// app.set("port", 443);
+
+// express의 미들웨어 설정
+//request에 대한 로그를 기록하는 미들웨어
 app.use(logger('dev'));
+
+// CORS 전부 오픈
+app.use(cors());
+
+// request 본문 해석용 미들웨어
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// request의 쿠키 해석용 미들웨어
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -36,6 +50,11 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// 서버 설정
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중입니다.");
 });
 
 module.exports = app;
